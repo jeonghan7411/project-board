2e53b130d11d1ac781ebc8fd443c0eaedfe387ec
bangjeonghan <aufkrh@naver.com>
2024-01-01 22:44:35 +0900

    #12 - AuditingFields 추출
    생성장, 생성일시, 수정자, 수정일시는 반복적으로
    엔티티 클래스에 들어가는 요소이고,
    도메인과 직접 연관이 없는 요소이므로
    추출이 가능하다.
    
    `@MappedSuperclass`  이용하여 상속 방식으로 추출함
    
    그외 `@DateTimeFormat`요소 추가 및  jpa옵션 개선

--- src/main/java/com/fastcampus/projectboard/domain/Article.java
+++ src/main/java/com/fastcampus/projectboard/domain/Article.java
@@ -23,9 +23,8 @@ import java.util.Set;
         @Index(columnList = "createdAt"),
         @Index(columnList = "createdBy"),
 })
-@EntityListeners(AuditingEntityListener.class)
 @Entity
-public class Article {
+public class Article extends AuditingFields{
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY) // autoIncrement를 위해
@@ -47,10 +46,7 @@ public class Article {
     @OneToMany(mappedBy = "article", cascade = CascadeType.ALL)
     private final Set<ArticleComment> articleComments = new LinkedHashSet<>();
 
-    @CreatedDate @Column(nullable = false) private LocalDateTime createdAt; // 생성일시
-    @CreatedBy @Column(nullable = false , length = 100) private String createdBy; // 생성자
-    @LastModifiedDate @Column(nullable = false) private LocalDateTime modifiedAt; // 수정일시
-    @LastModifiedBy @Column(nullable = false , length = 100) private String modifiedBy; // 수정자
+
 
     //jpa entity 는 Hibernate 구현체를 사용하는 경우 기본 생성자를 가지고 있어야함
     //최소한 protected 로 열어 줘야함 private는 오류 발생
